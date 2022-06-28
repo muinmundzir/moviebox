00cf4921ed14cb11c5d50709d147cd06809bd4a1
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-06-29 06:29:05 +0700

    change svg to react component

---
+++ src/assets/icons/ChevronRight.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const ChevronRight = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.5 4.66668L13.3333 10.5L7.5 16.3333"
+        stroke="#B91C1C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
