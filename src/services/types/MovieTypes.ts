99e36f5e97c6323453b393fb4bede2d27839e445
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-07-01 09:20:05 +0700

    edit types

--- src/services/types/MovieTypes.ts
+++ src/services/types/MovieTypes.ts
@@ -2,6 +2,29 @@ export interface genreTypes {
   id: number
 }
 
+export interface genreWithNameTypes {
+  id: number
+  name: string
+}
+
+export interface productionProfileTypes {
+  id: number
+  logo_path: string
+  name: string
+  origin_country: string
+}
+
+export interface productionCountriesTypes {
+  iso_3166_1: string
+  name: string
+}
+
+export interface spokenLanguangeTypes {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
 export interface MovieTypes {
   adult: boolean
   backdrop_path: string
@@ -19,3 +42,37 @@ export interface MovieTypes {
   vote_average: number
   vote_count: number
 }
+
+export interface MovieDetails {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: {
+    id: number
+    name: string
+    poster_path: string
+    backdrop_path: string
+  }
+  budget: number
+  genres: genreWithNameTypes[]
+  homepage: string
+  id: number
+  imdb_id: string
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: productionProfileTypes[]
+  production_countries: productionCountriesTypes[]
+  release_date: string
+  revenue: number
+  runtime: number
+  spoken_languages: spokenLanguangeTypes[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
