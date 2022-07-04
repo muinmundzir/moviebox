ef4443a75e958b2b9303b79cfd3624ba3b7dc58a
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-07-04 08:09:41 +0700

    add useScroll hooks

---
+++ src/services/hooks/useScroll.tsx
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
+
+import useIsMobile from './useIsMobile'
+
+export const useScroll = () => {
+  const [scroll, setScroll] = useState(false)
+  const isMobile = useIsMobile()
+  const location = useLocation()
+  const detailPage =  location.pathname.includes('/movie/')
+
+  const changeBackground = useCallback(() => {
+    if(detailPage) {
+      return window.scrollY >= 351 ? setScroll(true) : setScroll(false)
+    }
+    if (isMobile) {
+      return window.scrollY >= 748 ? setScroll(true) : setScroll(false)
+    }
+    return window.scrollY >= 600 ? setScroll(true) : setScroll(false)
+  }, [isMobile, detailPage])
+
+  useEffect(() => {
+    changeBackground()  
+
+    window.addEventListener('scroll', changeBackground)
+
+    return (() => window.removeEventListener('scroll', changeBackground))
+  }, [changeBackground])
+
+  return scroll
+}
