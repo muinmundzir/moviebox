cb5a9f6561b3daff59de79824fd2a4799ef75a5b
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-07-07 14:57:49 +0700

    edit barrel import

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,9 @@ export { Button } from 'components/Button/Button'
 
 export { List } from 'components/Container/List'
 export { HeroContainer } from 'components/Hero/HeroContainer'
+export { MoviesContainer} from 'components/Container/MoviesContainer'
+export { VideosContainer} from 'components/Container/VideosContainer'
+export { CastsContainer} from 'components/Container/CastsContainer'
+export { SearchMoviesContainer} from 'components/Container/SearchMoviesContainer'
 
 export { MovieInfo } from 'components/Movie/MovieInfo'
